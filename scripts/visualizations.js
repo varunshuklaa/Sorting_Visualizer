b39b49608f567384225190504478066efad4326c
varunshuklaa <112884557+varunshuklaa@users.noreply.github.com>
2024-10-01 01:40:59 +0530

    Update visualizations.js

--- scripts/visualizations.js
+++ scripts/visualizations.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   VARUN SHUKLA
     
     *****************
 */
@@ -59,7 +59,7 @@ function enable_buttons()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   VARUN SHUKLA
     
     *****************
 */
